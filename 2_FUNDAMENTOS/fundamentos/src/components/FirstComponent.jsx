6364cd24093cc925016725088f8d6d42a109e28b
lucasdasilvabezerra <lucas.svbezerra@gmail.com>
2024-03-13 11:40:33 -0300

    react fundamentos

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,10 @@
+const FirstComponent = ()=>{
+    return(
+        <div>
+            <h1>Fundamentos em React</h1>
+            <h2>Meu primeiro componente!</h2>
+        </div>
+    )
+}
+
+export default FirstComponent
